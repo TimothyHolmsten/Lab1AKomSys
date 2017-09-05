9c0294137794c1c42b009563728fb5d67ee45c11
timothy <timhol@kth.se>
2017-09-05 12:32:55 +0200

    Added main

--- src/Client/Main.java
+++ src/Client/Main.java
@@ -4,4 +4,7 @@ package Client;
  * Created by timothy on 2017-09-05, Client.
  */
 public class Main {
+    public static void main(String[] args) {
+
+    }
 }
--- src/Server/Main.java
+++ src/Server/Main.java
@@ -4,4 +4,7 @@ package Server;
  * Created by timothy on 2017-09-05, Server.
  */
 public class Main {
+    public static void main(String[] args) {
+
+    }
 }
