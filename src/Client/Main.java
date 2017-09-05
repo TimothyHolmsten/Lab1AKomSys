2ee4620d710dd806052aa0199b9200e9170a1460
Henrik <hestrand@kth.se>
2017-09-05 15:18:37 +0200

    Scanner added

--- src/Client/Main.java
+++ src/Client/Main.java
@@ -1,19 +1,20 @@
 package Client;
 
-/**
- * Created by timothy on 2017-09-05, Client.
- */
+import java.util.Scanner;
+
+
 public class Main {
+
     public static void main(String[] args) {
         Client c = new Client(4000, 5000);
         String initMsg = c.initialize();
         if(!initMsg.equals("BUSY")) {
             System.out.println(initMsg);
-            String message = "lol";/*
-            while(!message.equals("LOSE") || !message.equals("WIN")) {
-                // scanner input
-                // message = game(input)
-                // sout message
+            String message = "lol";
+            Scanner scanner = new Scanner(System.in);
+            /*while(!message.equals("LOSE") || !message.equals("WIN")) {
+                message = c.game(scanner.next());
+                //sysout
             }*/
             System.out.println(message);
         }
