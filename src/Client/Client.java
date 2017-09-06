8fe42b701a94c5993f7be02b76fd767e1bb885e0
Henrik <hestrand@kth.se>
2017-09-06 10:46:14 +0200

    Added comments

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -3,9 +3,6 @@ package Client;
 import java.io.IOException;
 import java.net.*;
 
-/**
- * Created by timothy on 2017-09-05, Client.
- */
 public class Client {
     private DatagramSocket clientSocket;
     private InetAddress address;
@@ -17,26 +14,20 @@ public class Client {
             clientSocket = new DatagramSocket();
         } catch (SocketException e) {
             e.printStackTrace();
+            System.err.println("Error: Connection was not possible");
         }
         try {
             address = InetAddress.getLocalHost();
         } catch (UnknownHostException e) {
             e.printStackTrace();
+            System.err.println("Error: The host is unknown");
         }
 
     }
-
-    public void sendMessage(String msg) {
-        byte[] buf = msg.getBytes();
-
-        DatagramPacket packet = new DatagramPacket(buf, buf.length, address, serverPort);
-        try {
-            clientSocket.send(packet);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
+    /**
+     * Initialiserar anslutningen till servern
+     * @return Returnerar om anslutningen till servern lyckas eller ej
+     */
     public String initialize() {
         byte[] buf = new byte[128];
         DatagramPacket rec = new DatagramPacket(buf, buf.length);
@@ -54,6 +45,11 @@ public class Client {
         return getMessageWithoutNull(rec);
     }
 
+    /**
+     * Klientents kommunikation till servern vid spelet.
+     * @param input Input med gissning från användaren.
+     * @return Returnerar svaret från servern.
+     */
     public String game(String input) {
         StringBuilder sb = new StringBuilder();
         byte[] buf = new byte[128];
@@ -70,6 +66,28 @@ public class Client {
         return sb.toString();
     }
 
+    /**
+     * Används för att skicka ett specifikt meddelanden.
+     * @param msg Meddelandet som ska skickas
+     *
+     */
+    public void sendMessage(String msg) {
+        byte[] buf = msg.getBytes();
+
+        DatagramPacket packet = new DatagramPacket(buf, buf.length, address, serverPort);
+        try {
+            clientSocket.send(packet);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Error: The message was not sent");
+        }
+    }
+
+    /**
+     * Tar emot ny data från servern.
+     * @param receive Tar emot ett datagrampacket
+     * @return Returnerar datan som finns i paketet
+     */
     private byte[] receiveData(DatagramPacket receive) {
         try {
             clientSocket.receive(receive);
@@ -79,6 +97,11 @@ public class Client {
         return receive.getData();
     }
 
+    /**
+     * Plockar ut datan från datagrampaketet
+     * @param packet Ett datagrampaket
+     * @return Returnerar en sträng av datan.
+     */
     private String getMessageWithoutNull(DatagramPacket packet) {
         return new String(packet.getData(), 0, packet.getLength());
     }
